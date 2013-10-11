e08d76ab6a90929fe2fa6d8e6fb2f314e7436487
Rob Huzzey <robert.huzzey@holidayextras.comalias>
2013-10-11 16:10:56 +0100

    Changed to use custom class names (defaults for now) for active & out states

--- jquery.navslider.js
+++ jquery.navslider.js
@@ -5,7 +5,11 @@ jQuery.fn.navSlider = function( options ) {
 		"content" : "#content",
 		"toggleNav" : "#burger",
 		"backButton" : ".back",
-		"contentPadding" : -30
+		"contentPadding" : -30, // TODO: try & see why this is -30 (countering CSS?)
+		"states" : {
+			"active" : "active",
+			"out" : "out" // TODO: Think of a better name for this
+		}
 	};
 
 	var settings = $.extend( {}, defaults, options );
@@ -23,6 +27,7 @@ jQuery.fn.navSlider = function( options ) {
 		var $contentContainer = $( settings.content );
 
 		var contentSlider = function( $contentContainer, x, y, z ) {
+			// TODO:  Need to put in x-browser transitions
 			$contentContainer.attr( "style", "-webkit-transform: translate3d(" + x + ", " + y + ", " + z + " );" );
 		}
 
@@ -35,11 +40,11 @@ jQuery.fn.navSlider = function( options ) {
 
 				// Moves content down dependant on nav menu height
 				contentSlider( $contentContainer, 0, ( $navMenu.height() + settings.contentPadding ) + "px", 0 );
-				$navMenu.addClass( "active" ).removeClass( "out" );
+				$navMenu.addClass( settings.states.active ).removeClass( settings.states.out );
 
 				// Remove active state on all sub menus
 				$( "ul", $topMenuItems ).each(function() {
-					$(this).removeClass( "active" ).removeClass( "out" );
+					$(this).removeClass( settings.states.active ).removeClass( settings.states.out );
 				});
 
 			} else { // Menu is active
@@ -47,11 +52,11 @@ jQuery.fn.navSlider = function( options ) {
 
 				// Moves content down dependant on nav menu height
 				contentSlider( $contentContainer, 0, "0px", 0 );
-				$navMenu.removeClass( "active" ).removeClass( "out" );
+				$navMenu.removeClass( settings.states.active ).removeClass( settings.states.out );
 
 				// Remove active state on all sub menus and add class Out
 				$( "ul", $topMenuItems ).each(function() {
-					$(this).removeClass( "active" ).addClass( "out" );
+					$(this).removeClass( settings.states.active ).addClass( settings.states.out );
 				});
 			}
 		});
@@ -64,17 +69,17 @@ jQuery.fn.navSlider = function( options ) {
 			var $subMenu = $( "> ul", $self );
 
 			// Moves main menu out
-			$navMenu.addClass( "out" ).removeClass( "active" );
+			$navMenu.addClass( settings.states.out ).removeClass( settings.states.active );
 
 			// Sub menu is now active
-			$subMenu.addClass( "active" );
+			$subMenu.addClass( settings.states.active );
 
 			contentSlider( $contentContainer, 0, ( $subMenu.height() + settings.contentPadding ) + "px", 0 );
 
 			// Set the active state of sub menu when the back button of that menu is clicked
 			$( settings.backButton, $self ).on( "click", function( e ) {
-				$subMenu.removeClass( "active" );
-				$navMenu.addClass( "active" ).removeClass( "out" );
+				$subMenu.removeClass( settings.states.active );
+				$navMenu.addClass( settings.states.active ).removeClass( settings.states.out );
 				contentSlider( $contentContainer, 0, ( $navMenu.height() + settings.contentPadding ) + "px", 0 );
 				e.stopPropagation();
 			});
