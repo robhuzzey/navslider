dfda3a54b464e99594f1b411b48654cc03992b8d
Rob Huzzey <robert.huzzey@holidayextras.comalias>
2013-10-11 14:18:38 +0100

    Turned into jQuery plugin

--- jquery.navslider.js
+++ jquery.navslider.js
@@ -1,76 +1,87 @@
-$(function() {
+jQuery.fn.navSlider = function( options ) {
 
-	// Store the nav menu in a var for later
-	var $navMenu = $( "#mobNav" );
+	var defaults = {
+		"navMenu" : "#mobNav",
+		"content" : "#content",
+		"toggleNav" : "#burger",
+		"backButton" : ".back",
+		"contentPadding" : -30
+	};
 
-	// Grab all our menu items ready for use later
-	var $topMenuItems = $( "> ul > li", $navMenu );
+	var settings = $.extend( {}, defaults, options );
 
-	var isNavMenuActive = 0;
-	//Set variable used for padding on the content
-	var contentPadding = -30;
+	return this.each( function() {
 
-	var $contentContainer = $( "#content" );
+		// Store the nav menu in a var for later
+		var $navMenu = $( this );
 
-	var contentSlider = function( $contentContainer, x, y, z ) {
-		$contentContainer.attr( "style", "-webkit-transform: translate3d(" + x + ", " + y + ", " + z + " );" );
-	}
+		// Grab all our menu items ready for use later
+		var $topMenuItems = $( "> ul > li", $navMenu );
 
-	// Toggle the active state of the menu
-	$( "#burger, .mobNavClose" ).on( "click", function() {
+		var isNavMenuActive = 0;
 
-		//If statement to check if menu is active
-		if( isNavMenuActive  === 0 ) { // Menu is inactive
-			isNavMenuActive = 1; // Set Menu Active
+		var $contentContainer = $( settings.content );
 
-			// Moves content down dependant on nav menu height
-			contentSlider( $contentContainer, 0, ( $navMenu.height() + contentPadding ) + "px", 0 );
-			$navMenu.addClass( "active" ).removeClass( "out" );
-
-			// Remove active state on all sub menus
-			$( "ul", $topMenuItems ).each(function() {
-				$(this).removeClass( "active" ).removeClass( "out" );
-			});
-
-		} else { // Menu is active
-			isNavMenuActive = 0; //Set Menu inactive
-
-			// Moves content down dependant on nav menu height
-			contentSlider( $contentContainer, 0, "0px", 0 );
-			$navMenu.removeClass( "active" ).removeClass( "out" );
-
-			// Remove active state on all sub menus and add class Out
-			$( "ul", $topMenuItems ).each(function() {
-				$(this).removeClass( "active" ).addClass( "out" );
-			});
+		var contentSlider = function( $contentContainer, x, y, z ) {
+			$contentContainer.attr( "style", "-webkit-transform: translate3d(" + x + ", " + y + ", " + z + " );" );
 		}
-	});
 
-	// When clicking a top level menu, set the active state of the sub menus
-	$topMenuItems.on( "click", function( e ) {
+		// Opens or closes the whole navigation menu
+		$( settings.toggleNav ).on( "click", function() {
 
-		// Store some lookups for easy use
-		var $self = $(this);
-		var $subMenu = $( "> ul", $self );
+			//If statement to check if menu is active
+			if( isNavMenuActive  === 0 ) { // Menu is inactive
+				isNavMenuActive = 1; // Set Menu Active
 
-		// Moves main menu out
-		$navMenu.addClass( "out" ).removeClass( "active" );
+				// Moves content down dependant on nav menu height
+				contentSlider( $contentContainer, 0, ( $navMenu.height() + settings.contentPadding ) + "px", 0 );
+				$navMenu.addClass( "active" ).removeClass( "out" );
 
-		// Sub menu is now active
-		$subMenu.addClass( "active" );
+				// Remove active state on all sub menus
+				$( "ul", $topMenuItems ).each(function() {
+					$(this).removeClass( "active" ).removeClass( "out" );
+				});
 
-		contentSlider( $contentContainer, 0, ( $subMenu.height() + contentPadding + 30) + "px", 0 );
+			} else { // Menu is active
+				isNavMenuActive = 0; //Set Menu inactive
 
-		// Set the active state of sub menu when the back button of that menu is clicked
-		$( ".back", $self ).on( "click", function( e ) {
-			$subMenu.removeClass( "active" );
-			$navMenu.addClass( "active" ).removeClass( "out" );
-			contentSlider( $contentContainer, 0, ( $navMenu.height() + contentPadding ) + "px", 0 );
-			e.stopPropagation();
+				// Moves content down dependant on nav menu height
+				contentSlider( $contentContainer, 0, "0px", 0 );
+				$navMenu.removeClass( "active" ).removeClass( "out" );
+
+				// Remove active state on all sub menus and add class Out
+				$( "ul", $topMenuItems ).each(function() {
+					$(this).removeClass( "active" ).addClass( "out" );
+				});
+			}
 		});
 
-		e.stopPropagation();
+		// When clicking a top level menu, set the active state of the sub menus
+		$topMenuItems.on( "click", function( e ) {
 
+			// Store some lookups for easy use
+			var $self = $(this);
+			var $subMenu = $( "> ul", $self );
+
+			// Moves main menu out
+			$navMenu.addClass( "out" ).removeClass( "active" );
+
+			// Sub menu is now active
+			$subMenu.addClass( "active" );
+
+			contentSlider( $contentContainer, 0, ( $subMenu.height() + settings.contentPadding ) + "px", 0 );
+
+			// Set the active state of sub menu when the back button of that menu is clicked
+			$( settings.backButton, $self ).on( "click", function( e ) {
+				$subMenu.removeClass( "active" );
+				$navMenu.addClass( "active" ).removeClass( "out" );
+				contentSlider( $contentContainer, 0, ( $navMenu.height() + settings.contentPadding ) + "px", 0 );
+				e.stopPropagation();
+			});
+
+			e.stopPropagation();
+
+		});
 	});
 
-});
+};
